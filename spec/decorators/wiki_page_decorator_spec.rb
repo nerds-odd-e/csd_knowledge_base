f85a658da718fcdf448481286b2f7f5e75589387
Naoto Yoshioka / Taiji Fukaya <yos@pb3.so-net.ne.jp>
2018-06-05 15:33:48 +0900

    found exact option

--- spec/decorators/wiki_page_decorator_spec.rb
+++ spec/decorators/wiki_page_decorator_spec.rb
@@ -34,4 +34,35 @@ describe WikiPageDecorator, type: :decorator do
       }
     end
   end
+
+  context 'the page with a link and text' do
+    before { subject.body = '[[link|text]]' }
+    its(:render_body) { should have_link('text', exact: true) }
+    # its(:render_body) { should have_link(href: h.wiki_space_wiki_page_path(subject.wiki_space, 'link')) }
+    # its(:render_body) { should have_link(class: 'absent') }
+    # its(:render_body) {
+    #   should have_link(
+    #     'text',
+    #     href:h.wiki_space_wiki_page_path(subject.wiki_space, 'link'),
+    #     class: 'absent'
+    #   )
+    # }
+
+    # context 'when the linked page exists' do
+    #   before { create :wiki_page, path: 'link', wiki_space: subject.wiki_space }
+    #   its(:render_body) { should_not have_link(
+    #     'text',
+    #     href:h.wiki_space_wiki_page_path(subject.wiki_space, 'link'),
+    #     class: 'absent'
+    #     )
+    #   }
+    #   its(:render_body) { should have_link(
+    #     'text',
+    #     href:h.wiki_space_wiki_page_path(subject.wiki_space, 'link'),
+    #     class: 'internal'
+    #     )
+    #   }
+    # end
+  end
+
 end
