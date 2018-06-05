b7d175c89555ced1e2bfbc00c3fff4f3e58c32ae
Naoto Yoshioka / Taiji Fukaya <yos@pb3.so-net.ne.jp>
2018-06-05 14:52:33 +0900

    more strict test

--- spec/decorators/wiki_page_decorator_spec.rb
+++ spec/decorators/wiki_page_decorator_spec.rb
@@ -10,11 +10,28 @@ describe WikiPageDecorator, type: :decorator do
     its(:render_body) { should have_link('something here') }
     its(:render_body) { should have_link(href: h.wiki_space_wiki_page_path(subject.wiki_space, 'something here')) }
     its(:render_body) { should have_link(class: 'absent') }
+    its(:render_body) {
+      should have_link(
+        'something here',
+        href:h.wiki_space_wiki_page_path(subject.wiki_space, 'something here'),
+        class: 'absent'
+      )
+    }
 
     context 'when the linked page exists' do
       before { create :wiki_page, path: 'something here', wiki_space: subject.wiki_space }
-      its(:render_body) { should_not have_link(class: 'absent') }
-      its(:render_body) { should have_link(class: 'internal') }
+      its(:render_body) { should_not have_link(
+        'something here',
+        href:h.wiki_space_wiki_page_path(subject.wiki_space, 'something here'),
+        class: 'absent'
+        )
+      }
+      its(:render_body) { should have_link(
+        'something here',
+        href:h.wiki_space_wiki_page_path(subject.wiki_space, 'something here'),
+        class: 'internal'
+        )
+      }
     end
   end
 end
