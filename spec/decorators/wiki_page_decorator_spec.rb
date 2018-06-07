e20608025974663359386e869797c05f50d7921d
Hironori Ogawa <pooh_mtb@hotmail.com>
2018-06-07 18:18:26 +0900

    refactor

--- spec/decorators/wiki_page_decorator_spec.rb
+++ spec/decorators/wiki_page_decorator_spec.rb
@@ -5,6 +5,46 @@ require 'rails_helper'
 describe WikiPageDecorator, type: :decorator do
   subject { create(:wiki_page).decorate }
 
+  test_cases_with_exist_check = [
+    { context: 'the page with link and label',
+      body: '[[link|text]]',
+      link_label: 'text',
+      link_page: 'link',
+      url: 'link' },
+    { context: 'アンカーが設定されているリンク',
+      body: '[[link#anchor]]',
+      link_label: 'link#anchor',
+      link_page: 'link',
+      url: 'link#anchor'},
+  ]
+  test_cases_with_exist_check.each do |test_case|
+    context test_case[:context] do
+      before { subject.body = test_case[:body] }
+      its(:render_body) {
+        should have_link(
+          test_case[:link_label],
+          href:h.wiki_space_wiki_page_path(subject.wiki_space, test_case[:url]),
+          class: 'absent'
+        )
+      }
+
+      context 'when the linked page exists' do
+        before { create :wiki_page, path: test_case[:link_page], wiki_space: subject.wiki_space }
+        its(:render_body) { should_not have_link(
+          test_case[:link_label],
+          href:h.wiki_space_wiki_page_path(subject.wiki_space, test_case[:url]),
+          class: 'absent'
+          )
+        }
+        its(:render_body) { should have_link(
+          test_case[:link_label],
+          href:h.wiki_space_wiki_page_path(subject.wiki_space, test_case[:url]),
+          class: 'internal'
+          )
+        }
+      end
+    end
+  end
   context 'the page with a link' do
     before { subject.body = '[[something here]]' }
     its(:render_body) {
@@ -53,7 +93,6 @@ describe WikiPageDecorator, type: :decorator do
     end
   end
 
-
   context 'アンカーが設定されているリンク' do
     before { subject.body = '[[link#anchor]]' }
     its(:render_body) {
@@ -101,33 +140,33 @@ describe WikiPageDecorator, type: :decorator do
   end
 
   test_cases = [
-    { context: 'wikispaceの中にあるwikipageへのリンク', 
+    { context: 'wikispaceの中にあるwikipageへのリンク',
       body: '[[wikispace:wikipage]]',
       link_label: 'wikipage',
       url: 'wikispace/wikipage'},
     { context: 'スラッシュ付きでwikipageへのリンク',
-      body: '[[wikispace/wikipage|wikipage]]', 
-      link_label: 'wikipage', 
+      body: '[[wikispace/wikipage|wikipage]]',
+      link_label: 'wikipage',
       url: 'wikispace/wikipage'},
-    { context: 'エスケープされたパイプの前にバックスラッシュがある', 
-      body: '[[link\\\\|text]]', 
-      link_label: 'text', 
+    { context: 'エスケープされたパイプの前にバックスラッシュがある',
+      body: '[[link\\\\|text]]',
+      link_label: 'text',
       url: 'link\\'},
-    { context: 'パイプがエスケープされている', 
-      body: '[[link\\|text]]', 
-      link_label: 'link|text', 
+    { context: 'パイプがエスケープされている',
+      body: '[[link\\|text]]',
+      link_label: 'link|text',
       url: 'link|text'},
-    { context: 'リンク先が設定されていないエイリアス', 
-      body: '[[|text]]', 
-      link_label: '|text', 
+    { context: 'リンク先が設定されていないエイリアス',
+      body: '[[|text]]',
+      link_label: '|text',
       url: '|text'},
-    { context: 'エイリアスが設定されていない', 
-      body: '[[link|]]', 
-      link_label: 'link|', 
+    { context: 'エイリアスが設定されていない',
+      body: '[[link|]]',
+      link_label: 'link|',
       url: 'link|'},
-    { context: 'パイプが二つ設定されている', 
-      body: '[[link||]]', 
-      link_label: 'link||', 
+    { context: 'パイプが二つ設定されている',
+      body: '[[link||]]',
+      link_label: 'link||',
       url: 'link||'},
     { context: 'エイリアスはあるがパイプが二つ設定されている', 
       body: '[[link||text]]', 
