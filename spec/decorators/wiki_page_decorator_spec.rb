41a5f6e15cfe273cdd18d8bdf4522691557232f8
Hironori Ogawa <pooh_mtb@hotmail.com>
2018-06-07 17:28:44 +0900

    remove duplicated tests

--- spec/decorators/wiki_page_decorator_spec.rb
+++ spec/decorators/wiki_page_decorator_spec.rb
@@ -34,9 +34,6 @@ describe WikiPageDecorator, type: :decorator do
 
   context 'the page with link and label' do
     before { subject.body = '[[link|text]]' }
-    its(:render_body) { should have_link('text', exact: true) }
-    its(:render_body) { should have_link(href: h.wiki_space_wiki_page_path(subject.wiki_space, 'link')) }
-    its(:render_body) { should have_link(class: 'absent') }
     its(:render_body) {
       should have_link(
         'text',
@@ -47,12 +44,6 @@ describe WikiPageDecorator, type: :decorator do
 
     context 'when the linked page exists' do
       before { create :wiki_page, path: 'link', wiki_space: subject.wiki_space }
-      its(:render_body) { should_not have_link(
-        'text',
-        href:h.wiki_space_wiki_page_path(subject.wiki_space, 'link'),
-        class: 'absent'
-        )
-      }
       its(:render_body) { should have_link(
         'text',
         href:h.wiki_space_wiki_page_path(subject.wiki_space, 'link'),
@@ -110,7 +101,6 @@ describe WikiPageDecorator, type: :decorator do
   end
 
   test_cases = [
-    # context, body, link_label, url
     {context: 'wikispaceの中にあるwikipageへのリンク', body: '[[wikispace:wikipage]]', link_label: 'wikipage', url: 'wikispace/wikipage'},
     {context: 'スラッシュ付きでwikipageへのリンク', body: '[[wikispace/wikipage|wikipage]]', link_label: 'wikipage', url: 'wikispace/wikipage'},
     {context: 'エスケープされたパイプの前にバックスラッシュがある', body: '[[link\\\\|text]]', link_label: 'text', url: 'link\\'},
